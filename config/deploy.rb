c221c12245e6ff500ff40c5bce1e3f4d519455e6
Brian <brian@insightify.com>
2010-06-19 14:36:19 -0700

    setup deploy

---
+++ config/deploy.rb
@@ -0,0 +1,11 @@
+set :repository,  "git@github.com:insightmethods/saintpatricksacademy.net.git"
+set :user,        "sites"
+set :domain,      "#{user}@insightmethods.com"
+set :deploy_to,   "/home/sites/saintpatricksacademy.net"
+set :port,          4000
+set :merb_env,      "preview"
+set :processes,     1
+
+namespace :vlad do
+
+end
